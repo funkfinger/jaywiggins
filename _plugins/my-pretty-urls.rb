8f8b36e8e4dc34c95710687775241662c5c2b4c3
Jay Wiggins <jay@irk.com>
2015-03-18 10:10:51 -0700

    added images

--- _plugins/my-pretty-urls.rb
+++ _plugins/my-pretty-urls.rb
@@ -1,17 +1,17 @@
-module Jekyll
-  class Post
-
-    def template_new
-      case site.permalink_style
-      when 'wp_legacy'
-        ":year/:month/:title/"
-      else
-        template_old
-      end
-    end
-  
-    alias_method :template_old, :template
-    alias_method :template, :template_new
-    
-  end
-end
+# module Jekyll
+#   class Post
+#
+#     def template_new
+#       case site.permalink_style
+#       when 'wp_legacy'
+#         ":year/:month/:title/"
+#       else
+#         template_old
+#       end
+#     end
+#
+#     alias_method :template_old, :template
+#     alias_method :template, :template_new
+#
+#   end
+# end
